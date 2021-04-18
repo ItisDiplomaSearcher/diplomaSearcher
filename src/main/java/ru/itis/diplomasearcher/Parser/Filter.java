7f6df6064a69c60e4034862c1451d6446a7bc018
danisimos <70813226+danisimos@users.noreply.github.com>
2021-04-18 12:36:46 +0300

    Add files via upload

---
+++ src/main/java/ru/itis/diplomasearcher/Parser/Filter.java
@@ -0,0 +1,14 @@
+package Parser;
+
+public class Filter {
+    private String title;
+    private String advisor;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAdvisor() {
+        return this.advisor;
+    }
+}
