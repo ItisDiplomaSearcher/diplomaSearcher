e66aea35f8ffed55fb921ec2a054b49658e35469
danisimos <70813226+danisimos@users.noreply.github.com>
2021-05-16 14:44:37 +0300

    Delete src/main/java/ru/itis/diplomasearcher/parser directory

--- src/main/java/ru/itis/diplomasearcher/parser/FilterSet.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.diplomasearcher.parser;
-
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-
-public abstract class FilterSet {
-    abstract SearchRequest getQuery();
-    abstract BoolQueryBuilder getBoolQueryBuilder();
-}
