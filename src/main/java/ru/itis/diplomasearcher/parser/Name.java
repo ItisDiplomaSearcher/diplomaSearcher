e32d21b7e452e5698e3c41521353f8c6e6296aa6
danisimos <70813226+danisimos@users.noreply.github.com>
2021-05-16 21:18:13 +0300

    Add files via upload

--- src/main/java/ru/itis/diplomasearcher/parser/Name.java
+++ src/main/java/ru/itis/diplomasearcher/parser/Name.java
@@ -1,10 +1,18 @@
 package ru.itis.diplomasearcher.parser;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum Name {
+    @SerializedName(value = "TITLE", alternate = "title")
     TITLE,
+    @SerializedName(value = "ADVISOR", alternate = "advisor")
     ADVISOR,
+    @SerializedName(value = "YEAR", alternate = "year")
     YEAR,
+    @SerializedName(value = "AUTHOR", alternate = "author")
     AUTHOR,
+    @SerializedName(value = "LEVEL", alternate = "level")
     LEVEL,
+    @SerializedName(value = "TEXT", alternate = "text")
     TEXT
 }
